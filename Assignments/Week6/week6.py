17db2bac226e44bfd7be8b507dd615d4b1880f3f
NothingHere2 <123258843+NothingHere2@users.noreply.github.com>
2023-02-13 19:00:29 -0500

    Update week6.py

--- Assignments/Week6/week6.py
+++ Assignments/Week6/week6.py
@@ -4,21 +4,28 @@ costPerCup = 0.20
 totalSales = 0
 totalProfit = 0.0
 
+
+
 # Complete the nested loop below so that the total number of cups sold is added to totalSales
 # Each list inside of dailyLemonadeSales can be considered a day with multiple sales recorded
-for #:
-    for #:
-        totalSales += #
+for day in dailyLemonadeSales:
+    for cupsSold in day:
+        totalSales += cupsSold
 else:
-  print(totalSales)
+    print(totalSales)
 
 # Complete the following code block to calculate the profit for each day, and add each value to 
 # the total profit. Remember to check the variables defined above to calculate profit!
-for #:
-    for #:
-        dailySales += # 
-    dailyTotalIncome = #
-    dailyOperatingCost = #
-    dailyProfit = round(#, 2)
+for day in dailyLemonadeSales:
+    dailyOperatingCost = 0.0
+    dailyTotalIncome = 0.0
+    dailyProfit = 0.0
+    dailySales = 0.0
+    for cupsSold in day:
+        dailySales += cupsSold
+    dailyOperatingCost = dailySales * costPerCup
+    dailyTotalIncome = dailySales * pricePerCup
+    dailyProfit = round(dailyTotalIncome - dailyOperatingCost, 2)
+    totalProfit += dailyProfit
     print(dailyProfit)
 print(totalProfit)
